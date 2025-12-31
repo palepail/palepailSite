b24eecd0d2541715c42d87b1df4db55d79030062
Derek Kam <derek@ehana.com>
2025-12-30 21:47:13 -1000

    updated tempalates

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,5 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+
+// For CI/CD environments, create src/environments/environment.ts with proper Firebase config
+// This import will fail if the file doesn't exist - CI/CD systems must create it
 import { environment } from '../environments/environment';
 
 const app = initializeApp(environment.firebase);
